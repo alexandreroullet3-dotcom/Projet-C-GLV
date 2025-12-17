5be5e72d83027e11961d3ef7dfffbab32dfdf374
Alexandre <alexandreroullet3@gmail.com>
2025-12-17 15:52:44 +0100

    ya l'algo deuclide mais il faut continuer la fontion principale

---
+++ methode_p_moins_1.c
@@ -0,0 +1,16 @@
+int factoriser_n (int n){
+     int a = 2;
+     for (int i=0; i < n; i++){
+        if 
+     }
+
+}
+
+int algorithme_d_euclide(int a, int b){
+    int r;
+    r = a%b
+    if (r==0){
+        return b;
+    }
+    return algorithme_d_euclide(b, r);
+}
