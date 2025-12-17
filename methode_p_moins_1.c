c7d38878d65d60249507230e6485e48a3cea088b
Alexandre <alexandreroullet3@gmail.com>
2025-12-17 16:00:44 +0100

    blk

--- methode_p_moins_1.c
+++ methode_p_moins_1.c
@@ -1,16 +1,31 @@
 int factoriser_n (int n){
      int a = 2;
-     for (int i=0; i < n; i++){
-        if 
+     for (int i = 2; i < n; i++){
+        int r = algorithme_d_euclide(a-1, n);
+        if ((r != 1) && (r != n)){
+            return r;
+        }
+        a = sq_mult(a, i);
      }
+}
 
+int sq_mult(int x, unsigned int d){
+    int temp=1; int puiss=x;
+    while (d!=0){
+        if (d&1){
+            temp = temp*puiss;
+        }
+        puiss = puiss*puiss;
+        d=d>>1;
+    } 
+    return temp;
 }
 
 int algorithme_d_euclide(int a, int b){
     int r;
-    r = a%b
+    r = a%b;
     if (r==0){
         return b;
-    }
+    };
     return algorithme_d_euclide(b, r);
 }
