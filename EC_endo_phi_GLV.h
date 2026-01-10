3b8132945321d2454ceb78d8e1da79d2caef2a8c
Gogeto <b.saudubray@gmail.com>
2026-01-10 15:55:05 +0100

    Ajout des fonction permettant de calculer k1 et k2

--- EC_endo_phi_GLV.h
+++ EC_endo_phi_GLV.h
@@ -5,4 +5,7 @@
 
 void ec_endo_phi_point_affine(ECPointAffine *Q, const ECPointAffine *P, const ECCurve *E, const mpz_t beta);
 
+void ec_glv_decompose(mpz_t k1, mpz_t k2, const mpz_t k, const mpz_t n, const mpz_t lambda,
+                    const mpz_t x1, const mpz_t y1, const mpz_t x2, const mpz_t y2);
+
 #endif
